418a857aef85931c15a59038b621378abd1a064c
tuesdaydude <ivanenko0607@gmail.com>
2021-06-10 11:06:07 -0800

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tkk_labs_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
